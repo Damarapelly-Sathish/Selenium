45fbc80b952d9d976eaf087675fb34938a1a20b4
Damarapelly-Sathish <168978053+Damarapelly-Sathish@users.noreply.github.com>
2024-08-29 13:33:35 +0530

    Added amazon testing

---
+++ src/main/java/Selenium/example/Selenium/AmazonWebTesting.java
@@ -0,0 +1,38 @@
+package Selenium.example.Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.core.joran.action.Action;
+
+@SpringBootApplication
+public class AmazonWebTesting {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(AmazonWebTesting.class, args);
+//		System.setProperty("webdriver.chrome.driver","E:\\SelemiumProject\\chromedriver-win64");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        Thread.sleep(3000);
+        WebElement elementToHover=driver.findElement(By.id("nav-link-accountList-nav-line-1"));
+        Actions action=new Actions(driver);
+        action.moveToElement(elementToHover).perform();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[contains(@class,\"nav-action-inner\")]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("ap_email")).sendKeys("19311a12c6@sreenidhi.edu.in");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("ap_password")).sendKeys("S@t26-26");
+        driver.findElement(By.id("signInSubmit")).click();
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("alba botanica");
+        driver.findElement(By.id("nav-search-submit-button")).click();
+        
+	}
+
+}
